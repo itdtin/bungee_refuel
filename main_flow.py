54f8bfaaee3f7422471cbef4ddd8eb3c4f51e159
Roman <45974112+itdtin@users.noreply.github.com>
2023-05-18 22:20:34 +0400

    fix wallets path

--- main_flow.py
+++ main_flow.py
@@ -4,6 +4,6 @@ from app.runner import Runner
 
 if __name__ == "__main__":
     path = pathlib.Path().resolve()
-    wallets_path = path.as_posix() + "/wallets.txt"
+    wallets_path = "wallets.txt"
     runner = Runner(wallets_path)
     runner.do_work()
